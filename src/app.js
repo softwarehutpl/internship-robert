fa9051bd73cdbeae057e758b09e774e0e3ad3f14
Robert Kulicki <robertkulicki1@wp.pl>
2017-07-19 15:53:50 +0200

    progress

--- src/app.js
+++ src/app.js
@@ -1,27 +1,11 @@
 import 'html-loader?interpolate!./index.html';
 
-import './scss/app.scss';
-
 import 'jquery';
 
+import './scss/app.scss';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
 import 'bootstrap';
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
